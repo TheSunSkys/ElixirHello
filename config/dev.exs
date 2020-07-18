5e0d18b815cac0be65358aaabcb71c741abc431c
TheSunSkys <thesunskys@gmail.com>
2020-07-18 21:11:24 +0700

    create data witch ecto create

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :hello2, Hello2.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "mysecretpassword",
   database: "hello2_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
